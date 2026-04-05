6762a64961336cc99c93b999a40ec25338051145
Denis <dposynko@gmail.com>
2026-04-05 05:38:50 +0000

    Add requirements and basic pytest

---
+++ app/tests/test_app.py
@@ -0,0 +1,7 @@
+from main import app
+
+def test_home():
+    client = app.test_client()
+    r = client.get("/")
+    assert r.status_code == 200
+    assert b"CI/CD Pipeline is working!" in r.data
